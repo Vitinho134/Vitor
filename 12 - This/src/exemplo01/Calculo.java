991dc3dc90da6e0fff7c8e0dabc3336bd585ff1d
Vitinho134 <39385785+Vitinho134@users.noreply.github.com>
2018-05-18 16:01:28 -0300

    This

---
+++ 12 - This/src/exemplo01/Calculo.java
@@ -0,0 +1,18 @@
+package exemplo01;
+
+public class Calculo {
+
+	//Atributo da classe
+	public double resultado = 10;
+	
+	//Método para exibir um resultado 
+	public void resultado(){
+		//Variaveis
+		double resultado = 5;
+		
+		//Exibir o resultado 
+		System.out.println(this.resultado);
+	}
+	
+	
+}
