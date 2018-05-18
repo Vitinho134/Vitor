d244603ca783dd7ea0be7b5a0d4cea50e32207be
Vitinho134 <39385785+Vitinho134@users.noreply.github.com>
2018-05-18 16:00:30 -0300

    Construtor

---
+++ 11 - Construtor/src/exemplo01/Principal.java
@@ -0,0 +1,11 @@
+package exemplo01;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa();
+		Pessoa p2 = new Pessoa("Vitinho");
+		Pessoa p3 = new Pessoa("Vitinho", 10);
+	}
+}
